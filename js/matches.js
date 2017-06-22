9d081592f81f025d2878310a85cff24b4e6f956f
Enock Mukhebi <mukhebienock@gmail.com>
2017-06-22 21:30:11 +0300

    add an on click effect to tournament and fixture

---
+++ js/matches.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+
+  $("#showfits").click(function() {
+    $("#fixts").show();
+    $("#tonament").hide();
+  });
+
+  $("#showtonas").click(function() {
+    $("#tonament").show();
+    $("#fixts").hide();
+  });
+})
